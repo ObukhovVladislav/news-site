e2ea01398e3b3af99768cc6832fca6522712f890
ObukhovVladislav <vobukhov10@gmail.com>
2021-10-16 11:45:39 +0500

    articleDetail

--- news_site/front/src/components/ArticleDetail.js
+++ news_site/front/src/components/ArticleDetail.js
@@ -6,9 +6,9 @@ const ArticleDetail = ({articles}) => {
     let article = articles.filter((item) => item.id === +id)[0];
     return (
         <div className={"article-detail"}>
+            <span>Date: {article.date}</span>
             <h2>Article: {article.title}</h2>
             <h4>Category: {article.category}</h4>
-            <span>Date: {article.date}</span>
             <p>Img: {article.img}</p>
             <p>Text: {article.text}</p>
         </div>
