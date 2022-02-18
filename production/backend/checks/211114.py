4d6e0a1830d9581246de3aaca8ebecbbfb525520
ObukhovVladislav <vobukhov10@gmail.com>
2022-02-18 22:09:25 +0500

    db postgres

--- production/backend/checks/211114.py
+++
@@ -1,7 +0,0 @@
-from requests import post
-
-url = 'http://localhost:8000/api-token-auth/'
-user_cred = {'username': 'admin', 'password': 'admin'}
-response = post(url, data=user_cred)
-resp_data = response.json()
-print(resp_data)
--- production/backend/news_site/settings.py
+++ production/backend/news_site/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'news_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'news-site',
+        'USER': 'admin',
+        'PASSWORD': 'admin123',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
