6a483909051e03323e1efa76586a0263defb9b2d
ObukhovVladislav <vobukhov10@gmail.com>
2022-02-18 18:00:31 +0500

    update

--- production/backend/news_site/settings.py
+++ production/backend/news_site/settings.py
@@ -134,8 +134,8 @@ AUTH_USER_MODEL = 'authapp.UserProfile'
 LOGIN_REDIRECT_URL = '/articles'
 
 CORS_ALLOWED_ORIGINS = [
-    'http://127.0.0.1:3000',
-    'http://localhost:3000',
+    'http://127.0.0.1:8000',
+    'http://localhost:8000',
 ]
 
 REST_FRAMEWORK = {
