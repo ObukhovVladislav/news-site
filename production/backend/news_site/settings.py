f4a748c5feba0e4d86a55c97e6d7fcda17e3b398
ObukhovVladislav <vobukhov10@gmail.com>
2022-02-26 09:31:27 +0500

    port_8081

--- production/backend/news_site/settings.py
+++ production/backend/news_site/settings.py
@@ -144,8 +144,8 @@ AUTH_USER_MODEL = 'authapp.UserProfile'
 LOGIN_REDIRECT_URL = '/articles'
 
 CORS_ALLOWED_ORIGINS = [
-    'http://127.0.0.1:8001',
-    'http://localhost:8001',
+    'http://127.0.0.1:8000',
+    'http://localhost:8000',
 ]
 
 REST_FRAMEWORK = {
